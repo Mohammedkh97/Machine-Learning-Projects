79b0bdf5506ed753c411ed9576fed1d00686e528
Mohammedkh97 <mohamedkhalaf20172020@gmail.com>
2025-07-24 17:08:52 +0400

    add views related to chatbot

--- Build RAG App From Scratch/support_bot/chatbot/views.py
+++ Build RAG App From Scratch/support_bot/chatbot/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-# from .rag import run_rag  # We'll create this next
+from .rag import run_rag
 
 
 def chat_view(request):
     answer = ""
     question = ""
 
-    # if request.method == "POST":
-    #     question = request.POST.get("question")
-    #     if question:
-    #         answer = run_rag(question)
+    if request.method == "POST":
+        question = request.POST.get("question")
+        if question:
+            answer = run_rag(question)
 
     return render(
         request,
